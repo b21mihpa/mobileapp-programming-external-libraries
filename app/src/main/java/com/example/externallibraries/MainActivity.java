0ff09308b6449f796a9a8575a95bc642c7cd8acb
b21mihpa <b21mihpa@student.his.se>
2022-05-19 14:46:27 +0200

    added: Glide and implemented click action

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -3,7 +3,13 @@ package com.example.externallibraries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +18,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final EditText editText = findViewById(R.id.editText);
         final Button button = findViewById(R.id.button);
+        final ImageView imageView = findViewById(R.id.imageView);
+
         button.setOnClickListener(view -> {
-            // TODO
+            if (editText.getText().toString().isEmpty()) {
+                Toast.makeText(this, "Please, enter a URL.", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            if (!Patterns.WEB_URL.matcher(editText.getText().toString()).matches()) {
+                Toast.makeText(this, "Please, enter a valid URL.", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            Glide.with(this).load(editText.getText().toString()).into(imageView);
         });
     }
 }
